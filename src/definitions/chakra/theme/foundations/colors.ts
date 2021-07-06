c73f74055747b1d1c36b1fbd4f7239ee4b341330
Muhammad Qasim <muhammad.qasim@accenture.com>
2021-07-06 17:22:09 +0100

    updated global style and color theme

--- src/definitions/chakra/theme/foundations/colors.ts
+++ src/definitions/chakra/theme/foundations/colors.ts
@@ -1,19 +1,5 @@
 export default {
     brand: {
-        900: "#1a365d",
-        800: "#153e75",
-        700: "#2a69ac",
-    },
-    header: {
-        100: "#20232a",
-    },
-    main: {
-        100: "#282c34",
-    },
-};
-
-export const colors = {
-    brand: {
         primary: "#2182BD",
         secondary: "#5282BD",
         muted: "#C6DAF7",
--- src/definitions/chakra/theme/styles.ts
+++ src/definitions/chakra/theme/styles.ts
@@ -5,6 +5,9 @@ type GlobalStyles = Pick<ThemeOverride, "styles">;
 export default {
     styles: {
         global: {
+            body: {
+                backgroundColor: "bg.primary",
+            },
             h1: {
                 fontWeight: 500,
                 marginBottom: "0.5em",
