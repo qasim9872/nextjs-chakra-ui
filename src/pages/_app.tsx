9c3154e620f0151c1a15bdac3b806050edba6f0c
Muhammad Qasim <qasim9872@gmail.com>
2021-07-06 20:02:05 +0100

    removing unused styles

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,7 +3,6 @@ import { AppProps } from "next/app";
 import { ChakraProvider } from "@chakra-ui/react";
 import theme from "@definitions/chakra/theme";
 import { AppLayout } from "src/layouts/app.layout";
-import "@styles/global.css";
 
 function MyApp({ Component, pageProps }: AppProps): JSX.Element {
     return (
