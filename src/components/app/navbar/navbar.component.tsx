561027b72e3ab6fea69ff96bb6fe1fddcf99ade8
Muhammad Qasim <qasim9872@gmail.com>
2021-07-06 23:17:03 +0100

    refactoring and removing navbar

--- src/components/app/navbar/navbar.component.tsx
+++ src/components/app/navbar/navbar.component.tsx
@@ -1,66 +1,53 @@
 import React from "react";
 import {
-    Flex,
-    Text,
-    Stack,
+    Box,
     Button,
     Collapse,
-    IconButton,
+    Flex,
+    Stack,
     useDisclosure,
-    useBreakpointValue,
 } from "@chakra-ui/react";
-import { HamburgerIcon, CloseIcon } from "@chakra-ui/icons";
 
-import { NavItem } from "../../../interfaces/nav-item";
-import { DesktopNav } from "./navbar-desktop.component";
-import { MobileNav } from "./navbar-mobile.component";
-import {
-    NavbarRow,
-    NavbarContainer,
-    IconContainerMobile,
-} from "./navbar.styles";
+import { NavItem } from "src/interfaces/nav-item";
+import { MenuToggle } from "./menu-toggle.component";
+import { MobileNav } from "./mobile/mobile-nav.component";
+import { DesktopNav } from "./desktop/desktop-nav.component";
 
-export const Navbar: React.FC<{ navItems: Array<NavItem> }> = ({
-    navItems,
-}) => {
+export const Navbar: React.FC<{
+    navItems: NavItem[];
+    Logo: React.ComponentType;
+}> = ({ navItems = [], Logo }) => {
     const { isOpen, onToggle } = useDisclosure();
 
     return (
-        <NavbarContainer>
-            <NavbarRow>
-                <IconContainerMobile>
-                    <IconButton
-                        onClick={onToggle}
-                        icon={
-                            isOpen ? (
-                                <CloseIcon w={3} h={3} />
-                            ) : (
-                                <HamburgerIcon w={5} h={5} />
-                            )
-                        }
-                        variant="ghost"
-                        aria-label="Toggle Navigation"
-                    />
-                </IconContainerMobile>
+        <Box as="nav">
+            <Flex
+                w="100%"
+                minH="60px"
+                py={{ base: 2 }}
+                px={{ base: 4 }}
+                borderBottom={1}
+                borderStyle="solid"
+                justifyContent="space-between"
+                backgroundColor="bg.primary"
+                borderColor="bg.secondary"
+            >
+                <Flex
+                    ml={{ base: -2 }}
+                    flex={{ base: 1, md: "auto" }}
+                    display={{ base: "flex", md: "none" }}
+                >
+                    <MenuToggle isOpen={isOpen} onToggle={onToggle} />
+                </Flex>
 
                 <Flex
                     flex={{ base: 1 }}
                     alignItems="center"
                     justify={{ base: "center", md: "start" }}
                 >
-                    <Text
-                        textAlign={useBreakpointValue({
-                            base: "center",
-                            md: "left",
-                        })}
-                        margin="0px"
-                        fontFamily={"heading"}
-                        color="gray.800"
-                    >
-                        Logo
-                    </Text>
+                    <Logo />
 
-                    <Flex display={{ base: "none", md: "flex" }} ml={10}>
+                    <Flex ml={10} display={{ base: "none", md: "flex" }}>
                         <DesktopNav navItems={navItems} />
                     </Flex>
                 </Flex>
@@ -70,26 +57,23 @@ export const Navbar: React.FC<{ navItems: Array<NavItem> }> = ({
                     justify={"flex-end"}
                     direction={"row"}
                     spacing={6}
+                    alignItems="center"
                 >
                     <Button
-                        display={{ base: "none", md: "inline-flex" }}
+                        href="/hello"
+                        variant="cta"
                         fontSize={"sm"}
                         fontWeight={600}
-                        color={"white"}
-                        bg={"pink.400"}
-                        href={"#"}
-                        _hover={{
-                            bg: "pink.300",
-                        }}
+                        display={{ base: "none", md: "inline-flex" }}
                     >
                         Get In Touch
                     </Button>
                 </Stack>
-            </NavbarRow>
+            </Flex>
 
             <Collapse in={isOpen} animateOpacity>
                 <MobileNav navItems={navItems} />
             </Collapse>
-        </NavbarContainer>
+        </Box>
     );
 };
--- src/layouts/app.layout.tsx
+++ src/layouts/app.layout.tsx
@@ -2,7 +2,6 @@ import React from "react";
 import { Flex } from "@chakra-ui/react";
 
 import { Footer } from "@components/app/footer/footer";
-import { Navbar as MyNavbar } from "@components/app/my-navbar/navbar.component";
 import { Navbar } from "@components/app/navbar/navbar.component";
 
 import { navItems } from "src/constants/nav.constants";
@@ -11,11 +10,7 @@ import { Logo } from "@components/app/logo/logo.component";
 export const AppLayout: React.FC = ({ children }) => {
     return (
         <Flex direction="column" minH="100vh">
-            <MyNavbar
-                navItems={navItems}
-                Logo={() => <Logo logoText="Logo" />}
-            />
-            <Navbar navItems={navItems} />
+            <Navbar navItems={navItems} Logo={() => <Logo logoText="Logo" />} />
 
             <Flex flexGrow={1}>{children}</Flex>
             <Footer />
