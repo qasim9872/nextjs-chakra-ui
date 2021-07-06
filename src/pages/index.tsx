58b8513cea32aca646cad94e4f8e735662e15173
Muhammad Qasim <muhammad.qasim@accenture.com>
2021-07-06 17:08:12 +0100

    removed unused components

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,10 @@
 import React from "react";
 import { Spacer, Flex } from "@chakra-ui/react";
 
-import { Main, Cards } from "@components";
-
 const Home: React.FC = () => {
     return (
         <Flex direction="column" minH="100vh">
-            <Main />
-            <Cards />
+            Hello From Home
             <Spacer />
         </Flex>
     );
