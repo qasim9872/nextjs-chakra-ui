2a57fcfd591dd1e52f8fed585fe439ad3391a162
Muhammad Qasim <muhammad.qasim@accenture.com>
2021-07-07 15:44:16 +0100

    added more header components

--- src/components/layouts/app.layout.tsx
+++ src/components/layouts/app.layout.tsx
@@ -6,11 +6,15 @@ import { Navbar } from "@components/sections/navbar/navbar.component";
 
 import { navItems } from "src/constants/nav.constants";
 import { Logo } from "@components/ui/logo/logo.component";
+import { Header as ChocHeader } from "@components/sections/header/header-choc.component";
+import { Header } from "@components/sections/header/header.component";
 
 export const AppLayout: React.FC = ({ children }) => {
     return (
         <Flex direction="column" minH="100vh">
-            <Navbar navItems={navItems} Logo={() => <Logo logoText="Logo" />} />
+            <ChocHeader />
+            {/* <Navbar navItems={navItems} Logo={() => <Logo logoText="Logo" />} />
+            <Header /> */}
 
             <Flex flexGrow={1}>{children}</Flex>
             <Footer />
